53224f09ac7ca464164d1a9b9881dce2fbdc6ac7
artem.chuzhmarov <artem.chuzhmarov@gmail.com>
2017-01-20 18:18:26 +0300

    fix feedback request

--- src/main/java/wtf/wtfgames/wtfwords/controller/type/FeedbackRequest.java
+++ src/main/java/wtf/wtfgames/wtfwords/controller/type/FeedbackRequest.java
@@ -2,9 +2,11 @@ package wtf.wtfgames.wtfwords.controller.type;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode(callSuper=true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class FeedbackRequest extends BaseIdRequest {
